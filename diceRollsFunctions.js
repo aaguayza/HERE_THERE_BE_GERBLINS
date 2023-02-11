cad3d0c80636bf18153f9e1a0de0e316d1830963
Tony <adaguayza@gmail.com>
2023-02-10 19:35:23 -0600

    "Rolling to attack functions are up and running"

--- diceRollsFunctions.js
+++ diceRollsFunctions.js
@@ -1,24 +1,40 @@
 //Simulate rolling a dice
 //add bonuses (whats your bonus)
 //show results
+//roll attack damage initial roll 
 const input = require('readline-sync');
-const rollDice = () =>{
-    const dice = Math.ceil(Math.random()*20);
-    return dice;
+
+const rollDice = (whatSidedDie, howMany = 1) =>{
+    const dieRolled = Math.ceil(Math.random()*whatSidedDie);
+    return dieRolled;
 };
 
-const attackRoll = () =>{
-    let attackBonus = Number(input.question('Attack Bonus: '))
-    let dice = rollDice();
-    let total;
+
+const attackRoll=(attackBonus = 2)=>{
+    const dieRolled = rollDice(12);
+    return dieRolled + attackBonus;
+} 
+
+
+const skillCheckRoll = (profeciencyBonus = 2,skillBonus = 1){
+    const dieRolled = rollDice(20);
+    return dieRolled + profeciencyBonus + skillBonus
+}
+
+const rollToWack = (hitBonus = 2) =>{
+    let dice = rollDice(20);
+    let damage = 0;
     if(dice === 20){
-        return `You rolled ${dice}\nCRITICAL HIT! Roll two dice\nDamage: ${dice += rollDice() + attackBonus}`;
+        console.log(`You rolled a ${dice}\nCRITICAL HIT!`);
+        return`You do ${attackRoll() + attackRoll(0)} to the enemy!`;
+        
     }else if(dice === 1){
-        return `You rolled ${dice}\nEPIC FAILURE!\nYou rolled a 1\nDamage: ${dice}`;
-    }
-    total = dice + attackBonus;
-    return `You rolled: ${dice}\nDamage: ${total}`;
+        console.log(`You rolled a ${dice}\nEPIC FAILURE!`);//pull from random shit top happen
+        return `Bad shit happens to you`
+    }else{
+    console.log(`You rolled a ${dice + hitBonus} to hit`)
+    return`You do ${attackRoll()} damage to the enemy!`
+};
 };
 
 
-console.log(attackRoll())
