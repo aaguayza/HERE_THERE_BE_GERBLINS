fa4256ef682880656a26ffd59dcd4678fa7893ca
Tony <adaguayza@gmail.com>
2023-02-13 14:40:54 -0600

    test commit

--- diceRollsFunctions.js
+++ diceRollsFunctions.js
@@ -70,20 +70,25 @@ console.log(rollToWack(2,2,12,1, practiceAc))
 // console.log(rollToWack(2,2,12,1,practiceAc))
 // The function above needs to be simplified a little more.  Just have it calculate to hit.
 //1d6 for damage +1 bonus
-// const enemyAttacks = (hitBonus = 1) =>{
-//     let dice = rollDice(20);
-//     if(dice === 20){
-//         console.log(`GERBLIN rolled a ${dice}\nCRITICAL HIT!`);
-//         return attackRoll(1,6) + attackRoll(0,6);
+//const rollToWack = (hitBonus = 2, damageBonus, damageDie, numberOfDamageDice,acToHit) =>{
+   //////WORK ON REDOING THE ATTACK FUNCTION
+    let totalToHit = rollDice();
+    if(totalToHit === 20){
+        console.log(`Enemy rolled a ${totalToHit}\nCRITICAL HIT!`);
+        return damageRoll(damageBonus,damageDie,numberOfDamageDice) + damageRoll(0,damageDie);
         
-//     }else if(dice === 1){
-//         console.log(`GERBLIN rolled a ${dice}\nEPIC FAILURE!`);//pull from random shit top happen
-//         return `Bad shit happens to the GERBLIN`
-//     }else{
-//     console.log(`GERBLIN rolled a ${dice + hitBonus} to hit`)
-//     return attackRoll(1,6)
-// }
-// };
+    }else if(totalToHit === 1){
+        console.log(`You rolled a ${totalToHit}\nEPIC FAILURE!`);//pull from random shit top happen
+        return `Bad shit happens to you`
+    }else{
+    console.log(`You rolled a ${totalToHit += hitBonus} to hit`)
+        if(totalToHit > acToHit){
+            return `You do ${damageRoll(damageBonus,damageDie, numberOfDamageDice)} damage`
+        }else{
+            return `Your attack missed!`
+}
+};
+}
 
 
 module.exports={
